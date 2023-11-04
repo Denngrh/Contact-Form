30571dc591b80bf2aac6fafc4c04d89ab8ee43c9
Denngrh <khansa0201ab@gmail.com>
2023-11-04 17:56:46 +0700

    fixed bug

--- index.php
+++ index.php
@@ -132,6 +132,26 @@ function create_table_custom_form()
     }
 }
 
+function drop_table_uninstall()
+{
+    global $wpdb;
+    $table1 = $wpdb->prefix . 'contact_form'; // Nama tabel pertama
+    $table2 = $wpdb->prefix . 'setting_form'; // Nama tabel kedua
+    $table3 = $wpdb->prefix . 'custom_form'; // Nama tabel ketiga
+
+    // Hapus tabel pertama dari database
+    $wpdb->query("DROP TABLE IF EXISTS $table1");
+
+    // Hapus tabel kedua dari database
+    $wpdb->query("DROP TABLE IF EXISTS $table2");
+
+    // Hapus tabel ketiga dari database
+    $wpdb->query("DROP TABLE IF EXISTS $table3");
+}
+
+// Uninstall
+register_uninstall_hook(__FILE__, "drop_table_uninstall");
+
 //custom_tabel
 register_activation_hook(__FILE__, 'create_table_custom_form');
 //setting tabel
